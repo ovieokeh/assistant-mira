d734f9a50545dd2ad0ee008a20315690137c88bf
Ovie Okeh <kevinokeh@gmail.com>
2023-04-06 09:48:06 +0200

    fix 500 google auth issue

--- app/routes/authorise_google.ts
+++ app/routes/authorise_google.ts
@@ -8,29 +8,24 @@ import { saveGoogleOAuthTokens } from '~/services/google.server';
 import { requireUser } from '~/services/session.server';
 
 export async function loader({ request }: LoaderArgs) {
-  try {
-    // read google oauth code from query params
-    const url = new URL(request.url);
-    const code = url.searchParams.get('code');
-    if (!code) {
-      return json({ error: 'no code' }, { status: 400 });
-    }
-
-    const user = (await requireUser(request, request.url)) as User;
-
-    //update user with google oauth code
-    await saveUserGoogleOAuthTokens({
-      userId: user.id,
-      tokens: {
-        authCode: code,
-      },
-    });
-
-    await saveGoogleOAuthTokens({ userId: user.id });
-
-    return redirect('/dashboard');
-  } catch (error) {
-    console.error(error);
-    return json({ error: 'server error' }, { status: 500 });
+  // read google oauth code from query params
+  const url = new URL(request.url);
+  const code = url.searchParams.get('code');
+  if (!code) {
+    return json({ error: 'no code' }, { status: 400 });
   }
+
+  const user = (await requireUser(request, request.url)) as User;
+
+  //update user with google oauth code
+  await saveUserGoogleOAuthTokens({
+    userId: user.id,
+    tokens: {
+      authCode: code,
+    },
+  });
+
+  await saveGoogleOAuthTokens({ userId: user.id });
+
+  return redirect('/dashboard');
 }
